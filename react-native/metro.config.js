ceb35b8fa06cdeded08f3aa6d8f5b5a1ac7149f4
Gustav <71986914+froggygoggy@users.noreply.github.com>
2026-03-26 10:50:56 +0100

    first app is running

--- react-native/metro.config.js
+++ react-native/metro.config.js
@@ -1,3 +1,17 @@
-const {getDefaultConfig} = require('@react-native/metro-config');
+const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..');
+
+const config = {
+  watchFolders: [workspaceRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(projectRoot), config);
